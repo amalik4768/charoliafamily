df17228d30d3b66f537c1773f7bd939940f8ea35
amalik4768 <amalik4768@yahoo.com>
2025-08-02 10:43:40 -0500

    Add files via upload

--- treeData.js
+++ treeData.js
@@ -1,19 +1,17 @@
-// Edit this file to update your family structure
 const familyTreeData = {
-  name: "Ancestor Name",
-  children: [
-    {
-      name: "Child 1",
-      children: [
-        { name: "Grandchild 1" },
-        { name: "Grandchild 2" }
-      ]
-    },
-    {
-      name: "Child 2",
-      children: [
-        { name: "Grandchild 3" }
-      ]
-    }
-  ]
+    name: "Wazir Ali Charolia",
+    Spouse: [
+        {
+            name: "Maru Ben Charolia",
+            Son: [
+                { name: "Kassam Charolia" },
+                { name: "Akbar Charolia" },
+                { name: "Noorali Charolia" },
+                { name: "Sultan Charolia" }
+            ]
+      
+           Daughter name: "Amina Charolia"
+                { name: "Kulsum Charolia" },
+    ]
 };
+
